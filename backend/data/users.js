0c493f86c479d4a1a58e2977ecd6d610c826d9b4
phungdpham <phung.tech@gmail.com>
2021-03-09 03:13:19 -0600

    add user authorization in back-end

--- backend/data/users.js
+++ backend/data/users.js
@@ -11,11 +11,13 @@ const users = [
     name: "John Doe",
     email: "john@example.com",
     password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
   },
   {
     name: "Jane Do",
     email: "jane@example.com",
     password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
   },
 ];
 
