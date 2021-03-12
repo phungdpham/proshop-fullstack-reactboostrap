40ec9411d682fae964721c159c91f318560d2c1f
phungdpham <phung.tech@gmail.com>
2021-03-12 04:58:24 -0600

    front end authorization completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile } from "../controllers/userController.js";
-import { protect } from '../middleware/authMiddleware.js'
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
-router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
-
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
