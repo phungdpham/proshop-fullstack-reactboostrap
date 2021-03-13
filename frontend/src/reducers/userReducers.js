0dce7a2b8a78982ad2603a31cda3a8c9b1a932ed
phungdpham <phung.tech@gmail.com>
2021-03-13 02:48:44 -0600

    fixed bug

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -11,6 +11,7 @@ import {
   USER_REGISTER_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_RESET,
   USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants";
 
@@ -42,7 +43,7 @@ export const userRegisterReducer = (state = {}, action) => {
   }
 };
 
-export const userDetailsReducer = (state = { user: {}}, action) => {
+export const userDetailsReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case USER_DETAILS_REQUEST:
       return { ...state, loading: true };
@@ -55,7 +56,7 @@ export const userDetailsReducer = (state = { user: {}}, action) => {
   }
 };
 
-export const userUpdateProfileReducer = (state = { }, action) => {
+export const userUpdateProfileReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_UPDATE_PROFILE_REQUEST:
       return { loading: true };
@@ -63,6 +64,8 @@ export const userUpdateProfileReducer = (state = { }, action) => {
       return { loading: false, success: true, userInfo: action.payload };
     case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: action.payload };
+    case USER_UPDATE_PROFILE_RESET:
+      return {};
     default:
       return state;
   }
